eb1ad5e65750924da96a915bd665aced0e5e589b
salehhashemi1992 <81674631+salehhashemi1992@users.noreply.github.com>
2023-04-30 00:32:24 +0430

    use commit sha to show the changes

--- main.php
+++ main.php
@@ -43,12 +43,13 @@ function main(): void
 {
     $committerEmail = getenv('INPUT_COMMITTER_EMAIL') ?: '';
     $committerName = getenv('INPUT_COMMITTER_NAME') ?: '';
+    $commitSha = getenv('GITHUB_SHA') ?: '';
 
     exec('git config --global --add safe.directory /github/workspace');
 
     $url = 'https://saleh-hashemi.ir/open-ai/commit-message';
     $commitTitle = exec('git log -1 --pretty=%s');
-    $commitChanges = exec('git show HEAD | tail -n +2 | head -n 50');
+    $commitChanges = exec('git show ' . $commitSha . ' | head -n 50');
 
     echo "Commit Title: " . $commitTitle . '\n';
     echo "Commit Changes: " . $commitChanges . '\n';
