2defc2e76f76e385d66f4712029677fa885fce8f
salehhashemi1992 <81674631+salehhashemi1992@users.noreply.github.com>
2023-04-30 16:57:17 +0430

    api call implementation

--- main.php
+++ main.php
@@ -2,81 +2,44 @@
 
 declare(strict_types=1);
 
+use GuzzleHttp\Client;
+
 require 'vendor/autoload.php';
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\GuzzleException;
-
-function getCommitDescription(string $url, string $title, string $changes): string
+function fetchAiGeneratedTitleAndDescription(string $commitChanges): array
 {
     $client = new Client();
+    $response = $client->post('https://saleh-hashemi.ir/open-ai/commit-message', [
+        'json' => ['commit_changes' => $commitChanges]
+    ]);
 
-    try {
-        $response = $client->request('POST', $url, [
-            'json' => [
-                'title' => $title,
-                'changes' => $changes,
-            ],
-        ]);
+    $responseData = json_decode((string)$response->getBody(), true);
 
-        if ($response->getStatusCode() === 200) {
-            $body = json_decode((string)$response->getBody(), true);
-            return $body['description'] ?? '';
-        }
-    } catch (GuzzleException $e) {
-        // Handle exceptions
-    }
-
-    return '';
+    return [$responseData['title'], $responseData['description']];
 }
 
-function amendCommitMessage(
-    string $commitTitle,
-    string $commitDescription,
+function updateLastCommitMessage(
+    string $newTitle,
+    string $newDescription,
     string $committerEmail,
     string $committerName
 ): void {
     exec("git config user.email '{$committerEmail}'");
     exec("git config user.name '{$committerName}'");
 
-    $commitTitle = escapeshellarg($commitTitle);
-    $commitDescription = escapeshellarg($commitDescription);
-    exec("git commit --amend -m {$commitTitle} -m {$commitDescription}");
+    $newTitle = escapeshellarg($newTitle);
+    $newDescription = escapeshellarg($newDescription);
 
-    exec("git push --force-with-lease");
+    // Soft reset to the previous commit
+    exec("git reset --soft HEAD~1");
 
-    exec("git config --unset user.email");
-    exec("git config --unset user.name");
-}
+    // Create a new commit with the updated title and description
+    exec("git commit -m {$newTitle} -m {$newDescription}");
 
-function newAmendCommitMessage(
-    string $commitSha,
-    string $commitTitle,
-    string $commitDescription,
-    string $committerEmail,
-    string $committerName
-): void {
-    exec("git config user.email '{$committerEmail}'");
-    exec("git config user.name '{$committerName}'");
+    // Push the changes to the remote repository
+    exec("git push origin --force");
 
-    // 1. Create a new branch at the commit you want to amend
-    $newBranch = 'temp_amend_branch';
-    exec("git checkout -b {$newBranch} {$commitSha}");
-
-    // 2. Amend the commit on the new branch
-    $commitTitle = escapeshellarg($commitTitle);
-    $commitDescription = escapeshellarg($commitDescription);
-    exec("git commit --amend -m {$commitTitle} -m {$commitDescription}");
-
-    // 3. Perform an interactive rebase to incorporate the amended commit into the main branch
-    exec("git checkout main");
-    exec("git rebase --onto {$newBranch} {$commitSha}^");
-
-    // 4. Push the changes to the remote repository
-    exec("git push");
-
-    // Clean up: delete the temporary branch and unset user.email and user.name
-    exec("git branch -D {$newBranch}");
+    // Clean up: unset user.email and user.name
     exec("git config --unset user.email");
     exec("git config --unset user.name");
 }
@@ -84,31 +47,19 @@ function newAmendCommitMessage(
 function main(): void
 {
     $commitSha = getenv('GITHUB_SHA') ?: '';
-
     exec('git config --global --add safe.directory /github/workspace');
 
-    $url = 'https://saleh-hashemi.ir/open-ai/commit-message';
     $commitTitle = exec('git log -1 --pretty=%s');
-    $commitChanges = exec('git show ' . $commitSha . ' | head -n 50');
+    $commitChanges = exec("git diff {$commitSha}~ {$commitSha} --unified=0");
 
-    // Get the committer's name and email from the commit
-    $committerName = exec("git log -1 --pretty=%cn {$commitSha}");
-    $committerEmail = exec("git log -1 --pretty=%ce {$commitSha}");
+    $committerName = exec("git log -1 --pretty=%cn $commitSha");
+    $committerEmail = exec("git log -1 --pretty=%ce $commitSha");
 
-    echo "Commit Email: " . $committerEmail . '\n';
-    echo "Commit Name: " . $committerName . '\n';
-    echo "Commit Title: " . $commitTitle . '\n';
-    echo "Commit Changes: " . $commitChanges . '\n';
-
-    newAmendCommitMessage($commitSha, 'test', 'test2', $committerEmail, $committerName);
-    /*    $commitDescription = getCommitDescription($url, $commitTitle, $commitChanges);
-
-        if ($commitDescription) {
-            echo "Generated commit description:\n";
-            echo $commitDescription;
-        } else {
-            echo "Failed to generate commit description.";
-        }*/
+    // Call the updateLastCommitMessage function with the correct arguments
+    if ($commitTitle === '[ai]') {
+        list($newTitle, $newDescription) = fetchAiGeneratedTitleAndDescription($commitChanges);
+        updateLastCommitMessage($newTitle, $newDescription, $committerEmail, $committerName);
+    }
 }
 
 main();
